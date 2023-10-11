c3b7cffed8dd5cb0acb18675b61afa0dc99a50db
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-10-12 01:23:36 +0200

    Work

--- index.mjs
+++ index.mjs
@@ -1,66 +1,232 @@
-import fs from 'fs'
+import * as fs from 'fs/promises'
 import { execSync } from 'child_process'
 import { processInstructions } from './processInstructions.js'
 import * as readline from 'node:readline/promises'
+import path from 'path'
+import crypto from 'crypto'
+import { readJSON } from '@sanjo/read-json'
+import { writeJSON } from '@sanjo/write-json'
+import { existsSync as doesFileExist, read } from 'fs'
 
-const filePath = process.argv[2]
+let filePath = process.argv[2]
+let session = await loadSession()
 
-if (!filePath) {
-  console.error('Please provide a path to a Markdown file.')
-  process.exit(1)
-}
-
-const fileContents = fs.readFileSync(filePath, 'utf-8')
-const actions = processInstructions(fileContents)
-
-const rl = readline.createInterface({
+const readLine = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
 
-// TODO: Make it possible to continue the last session. Ask if the user would like to continue the last session (if there was one).
+if (filePath) {
+  filePath = path.resolve(filePath)
+  const file = await createFileObject(filePath)
+  if (isSameFilePathAsInSession(file.path, session)) {
+    const isContinuingSession = await handleSessionContinuation()
 
-for (const action of actions) {
+    if (!isContinuingSession) {
+      await startWithANewSession(file)
+    }
+  } else {
+    await startWithANewSession(file)
+  }
+} else {
+  const isContinuingSession = await handleSessionContinuation()
+
+  if (!isContinuingSession) {
+    console.error('Please provide a path to a Markdown file.')
+    process.exit(1)
+  }
+}
+
+const fileContents = await fs.readFile(session.filePath, 'utf-8')
+
+const actions = processInstructions(fileContents)
+await saveSession(session)
+
+while (session.action < actions.length) {
+  const action = actions[session.action]
   if (action.type === 'instruction') {
     console.log(action.instruction)
-    await rl.question('Press enter to continue. ')
+    await readLine.question('Press enter to continue. ')
   } else if (action.type === 'information') {
     console.log(action.information)
   } else if (action.type === 'instructionToWriteCodeInAFile') {
     console.log(action.instruction)
-    const input = await rl.question(
-      'Would you like me to create the file? ([y]/n)'
-    )
-    if (!(input && input.toLowerCase() === 'n')) {
-      // fs.writeFileSync(action.filePath, '')
-      console.log(`File created: ${action.filePath}`)
-    }
-    await rl.question('Press enter to continue. ')
+    await createFile(action.filePath)
+    await readLine.question('Press enter to continue. ')
   } else if (action.type === 'runCommand') {
     await runCommand(action)
   } else if (action.type === 'writeCode') {
-    const filePath = action.filePath
-    const code = action.code
+    await createFile(action.filePath, action.code)
+  }
+  session.action++
+  await saveSession(session)
+}
 
-    const answer = await rl.question(`Overwriting ${filePath} ([y]/n): `)
-    const answerLowerCase = answer.toLowerCase()
-    if (answerLowerCase === 'y' || answerLowerCase === '') {
-      // TODO: Make sure that there is a backup.
-      // If the current version of the file is committed to Git it seems fine.
-      // Otherwise: create a backup.
-      // fs.writeFileSync(filePath, code)
-      // console.log(`Code written to file: ${filePath}`)
-    } else if (answerLowerCase === 'n') {
-      break
+await fs.rm(determineSessionFile())
+
+process.exit(0)
+
+async function startWithANewSession(file) {
+  session = await createNewSessionForFile(file)
+  printNewSession()
+}
+
+function printNewSession() {
+  console.log('Starting with a new session.')
+}
+
+async function createFile(filePath, content = null) {
+  let question = null
+  if (doesFileExist(filePath)) {
+    if (content) {
+      question = `Would you like me to overwrite the file "${filePath}" with the new code?`
+    }
+  } else {
+    question = `Would you like me to create the file "${filePath}"?`
+  }
+
+  if (question) {
+    await askQuestion(readLine, question, [
+      {
+        text: 'y',
+        isDefault: true,
+        async handler() {
+          await fs.mkdir(path.dirname(filePath), { recursive: true })
+          await fs.writeFile(filePath, content ?? '', { encoding: 'utf-8' })
+        },
+      },
+      {
+        text: 'n',
+        handler() {},
+      },
+    ])
+  }
+}
+
+async function createFileObject(filePath) {
+  const fileContents = await fs.readFile(filePath, 'utf-8')
+  return {
+    path: filePath,
+    contents: fileContents,
+    hash: crypto.createHash('md5').update(fileContents).digest('hex'),
+  }
+}
+
+async function loadSession() {
+  const sessionFilePath = determineSessionFile()
+  try {
+    return await readJSON(sessionFilePath)
+  } catch (error) {}
+  return createNewSession()
+}
+
+async function createNewSessionForFile(file) {
+  return {
+    filePath: file.path,
+    hash: file.hash,
+    action: 0,
+  }
+}
+
+async function handleSessionContinuation() {
+  let isContinuingSession
+  if (isValidSession(session)) {
+    let fileContents
+    try {
+      fileContents = await fs.readFile(session.filePath, 'utf-8')
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        return
+      } else {
+        throw error
+      }
+    }
+    const hash = crypto.createHash('md5').update(fileContents).digest('hex')
+    if (hash === session.hash) {
+      await askQuestion(
+        readLine,
+        `Would you like to continue the last session (file: "${
+          session.filePath
+        }", action: ${session.action + 1})?`,
+        [
+          {
+            text: 'y',
+            isDefault: true,
+            handler() {
+              isContinuingSession = true
+            },
+          },
+          {
+            text: 'n',
+            handler() {
+              isContinuingSession = false
+            },
+          },
+        ]
+      )
     }
   }
+
+  return isContinuingSession
+}
+
+function isSameFilePathAsInSession(filePath, session) {
+  return session.filePath && session.filePath === filePath
+}
+
+async function askQuestion(readLine, question, answers) {
+  const answersText = `(${answers.map(convertAnswerToString).join('/')})`
+  const questionText = `${question} ${answersText}: `
+  let handler
+  do {
+    const answer = await readLine.question(questionText)
+    const answerLowerCase = answer.toLowerCase()
+    handler = answers.find(answer => {
+      return (
+        (answer.isDefault && answerLowerCase === '') ||
+        answerLowerCase === answer.text.toLowerCase()
+      )
+    }).handler
+  } while (!handler)
+  return await handler()
+}
+
+function convertAnswerToString(answer) {
+  let text = answer.text
+  if (answer.isDefault) {
+    text = `[${text}]`
+  }
+  return text
+}
+
+function isValidSession(session) {
+  return (
+    typeof session.filePath === 'string' &&
+    typeof session.hash === 'string' &&
+    typeof session.action === 'number' &&
+    doesFileExist(session.filePath)
+  )
+}
+
+function createNewSession() {
+  return {
+    filePath: null,
+    hash: null,
+    action: null,
+  }
+}
+
+async function saveSession(session) {
+  const sessionFilePath = determineSessionFile()
+  await fs.mkdir(path.dirname(sessionFilePath), { recursive: true })
+  await writeJSON(sessionFilePath, session)
 }
 
 async function runCommand(action) {
   let thereWasAnError
   do {
     console.log(action.instruction)
-    const answer = await rl.question(
+    const answer = await readLine.question(
       `Run \`${action.command}\` ([y]/n/alternative command): `
     )
     const answerLowerCase = answer.toLowerCase()
@@ -86,4 +252,19 @@ async function runCommand(action) {
       }
     }
   } while (thereWasAnError)
+}
+
+function determineSettingsFolderPath() {
+  return path.join(
+    process.env.APPDATA ||
+      (process.platform === 'darwin'
+        ? process.env.HOME + '/Library/Preferences'
+        : process.env.HOME + '/.local/share'),
+    'Sanjo',
+    'do-steps'
+  )
+}
+
+function determineSessionFile() {
+  return path.join(determineSettingsFolderPath(), 'session.json')
 }
